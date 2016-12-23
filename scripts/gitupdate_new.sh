0cd4aab6447d5e0bc03d597d9274de1da6295892
Arjun Shrinivas <vmsnivas@gmail.com>
2016-12-23 05:29:48 +0000

    Modified gitupdate_new.sh

--- scripts/gitupdate_new.sh
+++ scripts/gitupdate_new.sh
@@ -6,35 +6,29 @@ echo
 
 if [[ -z $REPODIR ]]
 then
-	REPODIR="/e/Practice/GitRepos"
+	REPODIR="/GitHub"
 fi
 
-#for i in `echo $(ls $REPODIR | tr -d '/')`
-echo -e "Updating ACCOUNT\t\t\tPUSH-STATUS\t\tPULL-STATUS"
+echo "Updating ACCOUNT PUSH-STATUS PULL-STATUS" | awk '{printf "%-10s %-50s %-15s %-15s\n",$1,$2,$3,$4}'
 for i in `echo $(cd $REPODIR && find ./ -type d -name .git -exec echo {} \; | awk -F/.git$ '{print $1}' | awk -F^./ '{print $2}')`
 do
-#	echo "Changing to $REPODIR/$i"
 	cd $REPODIR/$i
 	GITURL=`git remote -v | grep fetch | awk '{print $2}'`
 	ACCOUNT=`echo $GITURL | awk -F.com/ '{print $2}' | awk -F.git$ '{print $1}'`
-#	echo "Updating $ACCOUNT ..."
-#	echo "Pulling all the changes from $GITURL <--- ..."
-	GPLOUT=$(git pull -ff origin master 2>&1)
-	if [[ ! -z $GPLOUT ]]
+	GPLOUT=$(git pull -ff origin master 2>&1 | grep up-to-date)
+	if [[ -z $GPLOUT ]]
 	then
-		PULLOUT="COMMITS PULLED"
+		PULLOUT="COMMITS-PULLED"
 	else
 		PULLOUT="UP-TO-DATE"
 	fi
 	PUSHCHK=`git lg | head -n1 | grep -v origin/master`
 	if [[ ! -z $PUSHCHK ]]
 	then
-#		echo "Pushing all local commits to $GITURL ---> ..."
 		GPSOUT=$(git push -fu origin master 2>&1)
-		PUSHOUT="COMMITS PUSHED"
+		PUSHOUT="COMMITS-PUSHED"
 	else
-#		echo "ORIGIN is up-to date. No commits found to push!"
 		PUSHOUT="UP-TO-DATE"
 	fi
-	echo -e "Updating $ACCOUNT\t\t$PUSHOUT\t\t$PULLOUT"
+	echo  "Updating $ACCOUNT $PUSHOUT $PULLOUT" | awk '{printf "%-10s %-50s %-15s %-15s\n",$1,$2,$3,$4}'
 done

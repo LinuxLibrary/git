6380a240845cfd23b10c358bd5378bae3245fa4d
Arjun Shrinivas <vmsnivas@gmail.com>
2017-01-11 08:11:24 +0000

    Modified scripts/05-gitupdate_new.sh

--- scripts/05-gitupdate_new.sh
+++ scripts/05-gitupdate_new.sh
@@ -9,26 +9,37 @@ then
 	REPODIR="/GitHub"
 fi
 
-echo "Updating ACCOUNT PUSH-STATUS PULL-STATUS" | awk '{printf "%-10s %-50s %-15s %-15s\n",$1,$2,$3,$4}'
+echo "Updating ACCOUNT PUSH-STATUS PULL-STATUS" | awk '{printf "%-10s %-65s %-15s %-15s\n",$1,$2,$3,$4}'
 for i in `echo $(cd $REPODIR && find ./ -type d -name .git -exec echo {} \; | awk -F/.git$ '{print $1}' | awk -F^./ '{print $2}')`
 do
 	cd $REPODIR/$i
 	GITURL=`git remote -v | grep fetch | awk '{print $2}'`
 	ACCOUNT=`echo $GITURL | awk -F.com/ '{print $2}' | awk -F.git$ '{print $1}'`
-	GPLOUT=$(git pull -ff origin master 2>&1 | grep up-to-date)
-	if [[ -z $GPLOUT ]]
+	GPLOUT=$(git pull -ff origin master 2>&1)
+	GPLSUCCESS=$(echo $GPLOUT | grep up-to-date)
+	GPLCOMMITS=$(echo $GPLOUT | grep Updating)
+	if [[ ! -z $GPLSUCCESS ]]
+	then
+		PULLOUT="UP-TO-DATE"
+	elif [[ ! -z $GPLCOMMITS ]]
 	then
 		PULLOUT="COMMITS-PULLED"
-	else
-		PULLOUT="UP-TO-DATE"
 	fi
 	PUSHCHK=`git lg | head -n1 | grep -v origin/master`
 	if [[ ! -z $PUSHCHK ]]
 	then
 		GPSOUT=$(git push -fu origin master 2>&1)
-		PUSHOUT="COMMITS-PUSHED"
+		GPSSUCCESS=$(echo $GPSOUT | grep "To.*git$")
+		GPSDENIED=$(echo $GPSOUT | grep "Permission.*denied")
+		if [[ ! -z $GPSSUCCESS ]]
+		then
+			PUSHOUT="COMMITS-PUSHED"
+		elif [[ ! -z $GPSDENIED ]]
+		then
+			PUSHOUT="ACCESS_DENIED"
+		fi
 	else
 		PUSHOUT="UP-TO-DATE"
 	fi
-	echo  "Updating $ACCOUNT $PUSHOUT $PULLOUT" | awk '{printf "%-10s %-50s %-15s %-15s\n",$1,$2,$3,$4}'
+	echo  "Updating $ACCOUNT $PUSHOUT $PULLOUT" | awk '{printf "%-10s %-65s %-15s %-15s\n",$1,$2,$3,$4}'
 done

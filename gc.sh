8eaa25dd148fa9023693cbec40fbad67e117a0e3
bgomkar <bgomkar@gmail.com>
2017-09-16 19:30:26 +0530

    Added gc.sh

---
+++ gc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {00..21}
+do
+	if [[ $(expr $i % 2) -eq 0 ]]
+	then
+		date -s "$(date --date="+1 day" +"%Y-%m-%d %H:%M:%S")" && mv orgcloneres.in orgcloneres.out && gitadd
+	elif [[ $(expr $i % 2) -ne 0 ]]
+	then
+		date -s "$(date --date="+1 day" +"%Y-%m-%d %H:%M:%S")" && mv orgcloneres.out orgcloneres.in && gitadd
+	fi
+done
